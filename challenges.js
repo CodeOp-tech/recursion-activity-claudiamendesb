1d6fb0dcda9006e91ec44d3e7460e9a88063e15e
claudia <claudiamendesbarros@hotmail.com>
2023-06-02 00:25:16 +0100

    up to exercise 6

--- challenges.js
+++ challenges.js
@@ -121,22 +121,35 @@ console.log(fib(3)); // 3
 console.log(fib(7)); // 21
 */
 /*------------------
-6)
+6)?
 Create a function that takes a grid
 as an argument and adds all the numbers
 in that grid together. The grid can have any number of nested arrays, with any possible depth.
 
-Test cases:
+Test cases:*/
+/*
 const y = [[[1], [2]]];
 console.log(addGridItems(y)); // 3
 const x = [[[1, 0, -3], [2, 4], [3]], [[4, 4, 3], [5, -3, -1], [6, 10]], [[7, -5, -5], [8, 0], [9, 1]]];
 console.log(addGridItems(x)); // 50
 const z = [[1, 0, -3, [2, 4], [3]], 4, [[4, 3], [[5, -3], -1], [6, [[[10]]]]], [[7, -5, -5], [8, 0], [9, 1]]];
 console.log(addGridItems(z)); // 50
-------------------*/
 
-function addGridItems(grid) {}
 
+function addGridItems(grid) {
+  if (Array.isArray(grid)) {
+    let sum = 0;
+    for  (let i = 0; i < grid.length; i++) {
+      sum += addGridItems(grid[i]);
+    }
+    return sum;
+  }else if (typeof grid === 'number') {
+    return grid;
+  } else{
+    return 0;
+  }
+}
+*/
 /*------------------
 7) Write a sortedInsert() function which inserts a node into 
 the correct location of a pre-sorted linked list which is sorted in ascending order
@@ -145,6 +158,7 @@ sortedInsert() should also return the head of the list.
 sortedInsert() should be able to insert a node into an empty list
 
 Test cases:
+
 const head1 = new Node(1, new Node(2, new Node(3)));
 const sorted1 = sortedInsert(head1, 4);
 console.log(
@@ -175,7 +189,7 @@ console.log(
 const head4 = null;
 const sorted4 = sortedInsert(head4, 1);
 console.log(sorted4.value); // 1
-------------------*/
+*/
 
 class Node {
   constructor(value, next = null) {
@@ -185,10 +199,58 @@ class Node {
 }
 
 function sortedInsert(head, value) {
+  const newNode = new Node(value);
+
+  if (head === null) {
+    return newNode;
+  }
+  if (value < head.value) {
+    newNode.next = head;
+    return newNode;
+  }
+  let current = head;
+  let previus = null;
+  while (current !== null && value >= current.value) {
+    previus = current;
+    current = current.next
+  }
+  previus.next = newNode;
+  newNode.next = current;
+
+  return head;
+
   // Your code goes here.
   // Remember to return the head of the list.
 }
+const head1 = new Node(1, new Node(2, new Node(3)));
+const sorted1 = sortedInsert(head1, 4);
+console.log(
+  sorted1.value,
+  sorted1.next.value,
+  sorted1.next.next.value,
+  sorted1.next.next.next.value
+); // 1 2 3 4
+const head2 = new Node(1, new Node(7, new Node(8)));
+const sorted2 = sortedInsert(head2, 5);
+console.log(
+  sorted2.value,
+  sorted2.next.value,
+  sorted2.next.next.value,
+  sorted2.next.next.next.value
+); // 1 5 7 8
 
+const head3 = new Node(3, new Node(5, new Node(9)));
+const sorted3 = sortedInsert(head3, 1);
+console.log(
+  sorted3.value,
+  sorted3.next.value,
+  sorted3.next.next.value,
+  sorted3.next.next.next.value
+); // 1 3 5 9
+
+const head4 = null;
+const sorted4 = sortedInsert(head4, 1);
+console.log(sorted4.value); // 1
 /*------------------
 7) Can you re-do exercises 1 to 5 using tail call optimization?
 - [] Read about tail call optimization:
